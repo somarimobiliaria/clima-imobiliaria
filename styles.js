465b4749464651085ba4df14583e3324d87ce320
somarimobiliaria <somarconsultoriaimobiliaria@gmail.com>
2024-12-17 12:03:09 -0300

    Add files via upload

---
+++ styles.js
@@ -0,0 +1,28 @@
+body {
+    margin: 0;
+    font-family: Arial, sans-serif;
+}
+
+#clima-panel {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background-color: #333;
+    color: #fff;
+    padding: 20px;
+}
+
+#clima-panel img {
+    width: 150px;
+    height: 150px;
+}
+
+@media screen and (max-width: 1080px) {
+    #clima-panel {
+        padding: 10px;
+    }
+}
